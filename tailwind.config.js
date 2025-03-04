d7890274ba6e6757df74ae7236a36e1749b8ec1b
Aung Min Myat <aungminmyat2212@gmail.com>
2025-03-05 01:36:42 +0630

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#696969",
+        secondary: "#60A5FA",
+        accent: "#F59E0B",
+      },
+    },
+  },
+  plugins: [],
+};
